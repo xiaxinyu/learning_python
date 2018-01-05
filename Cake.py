c6be8e6702e791a68232afedb0ebda0373cb3268
xiaxinyu <summer_west2010@126.com>
2018-01-05 15:42:20 +0800

    Add Guardian

--- Cake.py
+++
@@ -1,5 +0,0 @@
-def displayMovies(movies, level = 0, server="hostname"):
-    for movie in movies:
-        print(movie)
-    print(level)
-    print(server)
---
+++ Guardian.py
@@ -0,0 +1,21 @@
+'''
+Created on 2018.1.5
+
+@author: summer.xia
+@contact: summer_west2010@126.com
+'''
+import pickle
+
+try:
+    with open('readme.txt', 'wb') as mysavedata:
+        pickle._dump([1, 2, 'three'], mysavedata)
+    
+    with open('readme.txt', 'rb') as myrestoredata:
+        a_list = pickle.load(myrestoredata)
+    print(a_list)
+except IOError as err:
+    print('File error:' + str(err))
+except pickle.PickleError as perr:
+    print('Pickling error:' + str(perr))
+# if __name__ == '__main__':
+#     pass
