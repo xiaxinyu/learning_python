de3466063a4f72d76a8de67b1d043aa595f85c71
summer <summer@192.168.1.102>
2018-01-06 10:44:26 +0800

    update sorter

--- Sorter.py
+++ Sorter.py
@@ -5,7 +5,8 @@ Created on 2018.1.5
 @contact: summer_west2010@126.com
 '''
 import os
-import pickle
+from test.test_decimal import file
+
 
 def sanitize(time):
     if '-' in time:
@@ -33,41 +34,33 @@ def eliminateRepetition(items):
     return result
 
 
-def merge(aList, bList):
-    result = []
-    result.append(aList)
-    result.append(bList)
-    return result
-
-
 def report(aps, items):
     try:
         if os.path.exists(aps):
             os.remove(aps)
-        with open(aps, 'w') as mysavedata:
-            pickle._dump(items, mysavedata)
+        file = open(aps, 'w')
+        for key in items:
+            file.write(str(key) + ':' + str(items[key]) + '\n')    
     except IOError as err:
         print('File error:' + str(err))
-    except pickle.PickleError as perr:
-        print('Pickling error:' + str(perr))
+    finally:
+        if 'file' in locals():
+            file.close()
+
         
-            
-charles = getSourceData('Sorter\\charles.txt')
-james = getSourceData('Sorter\\james.txt')
-ray = getSourceData('Sorter\\ray.txt')
-summer = getSourceData('Sorter\\summer.txt')
-
-c_charles = eliminateRepetition(charles)
-c_james = eliminateRepetition(james)
-c_ray = eliminateRepetition(ray)
-c_summer = eliminateRepetition(summer)
-
-r_charles = sorted(c_charles[0:3])
-r_james = sorted(c_james[0:3])
-r_ray = sorted(c_ray[0:3])
-r_summer = sorted(c_summer[0:3])
-
-r_all = merge(merge(merge(r_charles, r_james), r_ray), r_summer)
-
-report('Sorter\\report.txt', r_all)
+if __name__ == '__main__':      
+    charles = getSourceData('Sorter/charles.txt')
+    james = getSourceData('Sorter/james.txt')
+    ray = getSourceData('Sorter/ray.txt')
+    summer = getSourceData('Sorter/summer.txt')    
+    c_charles = eliminateRepetition(charles)
+    c_james = eliminateRepetition(james)
+    c_ray = eliminateRepetition(ray)
+    c_summer = eliminateRepetition(summer)    
+    r_all = {}
+    r_all['c_charles'] = sorted(c_charles[0:3])
+    r_all['c_james'] = sorted(c_james[0:3])
+    r_all['c_ray'] = sorted(c_ray[0:3])
+    r_all['c_summer'] = sorted(c_summer[0:3])
+    report('Sorter/report.txt', r_all)
 
