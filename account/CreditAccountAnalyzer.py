c7a8c6d7b36a0ce58195565bcaab3adfff97794a
summer <summer@192.168.1.101>
2018-01-13 16:06:21 +0800

    add consumption type analyzer

--- account/CreditAccountAnalyzer.py
+++ account/CreditAccountAnalyzer.py
@@ -17,15 +17,18 @@ class CreditAccountAnalyzer(object):
     ctPath = os.path.join(BASE_DIR, 'account' + os.path.sep + 'static' + os.path.sep + 'consumption-type.json')
     descriptionColumnIndex = 5
     headerRowIndex = 0 
-    disbursementNewColumn1 = "支付渠道名称"
-    disbursementNewColumn2 = "支付渠道编码" 
-    typeOfUseNewColumn1 = "使用类型名称"
-    typeOfUseNewColumn2 = "使用类型编码" 
+    disbursementNewColumn1 = '支付渠道名称'
+    disbursementNewColumn2 = '支付渠道编码' 
+    typeOfUseNewColumn1 = '使用类型名称'
+    typeOfUseNewColumn2 = '使用类型编码' 
+    consumptionNewColumn1 = '消费类型名称'
+    consumptionNewColumn2 = '消费类型编码'
     
     def __init__(self, lines=[]):
         self.lines = lines
         self.dcData = self.listOrdinaryType(self.dcPath)
         self.touData = self.listOrdinaryType(self.touPath)
+        self.ctData = self.listConsumptionType(self.ctPath)
         
     def readDictionaryData(self, path):
         with codecs.open(path, 'r', self.encoding) as json_file:
@@ -41,17 +44,41 @@ class CreditAccountAnalyzer(object):
             if row['default']:
                 defaultRow = row
                 break
-        return {"default":defaultRow, "rows":data}
+        return {'default':defaultRow, 'rows':data}
     
     def listConsumptionType(self, path):
         data = self.readDictionaryData(path)
-        print(str(data))
+        rowData = None
+        allData = []
+        for firstNode in data:
+            allData = allData + firstNode['children']
+            for secondNode in firstNode['children'] :
+                if secondNode.get('default') is not None:
+                    if secondNode['default']:
+                        rowData = secondNode
+                        break
+        return {'default':rowData, 'rows':allData}
     
     def getOrdinaryType(self, text, data):
         result = data['default']
         for key in data['rows']:
             if key["name"] in text:
                 result = key
+                break
+        return result
+    
+    def getConsumptionType(self, text, data):
+        result = data['default']
+        for row in data['rows']:
+            if row.get('keyWords') is None:
+                continue
+            matchFlag = False
+            for keyWord in row['keyWords']:
+                if keyWord in text:
+                    result = row
+                    matchFlag = True
+                    break
+            if matchFlag:
                 break
         return result
     
@@ -64,6 +91,8 @@ class CreditAccountAnalyzer(object):
                 line.append(self.disbursementNewColumn2)
                 line.append(self.typeOfUseNewColumn1)
                 line.append(self.typeOfUseNewColumn2)
+                line.append(self.consumptionNewColumn1)
+                line.append(self.consumptionNewColumn2)
                 continue
             if line is None:
                 continue 
@@ -74,8 +103,13 @@ class CreditAccountAnalyzer(object):
             tou = self.getOrdinaryType(description, self.touData)
             line.append(tou['name'])
             line.append(tou['value'])
+            ct = self.getConsumptionType(description, self.ctData)
+            if ct is not None:
+                line.append(ct['name'])
+                line.append(ct['value'])
         return lines
 
-if __name__ == '__main__':
-    a = CreditAccountAnalyzer()
-    a.listConsumptionType(a.ctPath)
+
+# if __name__ == '__main__':
+#     a = CreditAccountAnalyzer()
+#     a.listConsumptionType(a.ctPath)
