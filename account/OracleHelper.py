66652dc660692ebabf2c96697883e4c39b5c630d
xiaxinyu <summer_west2010@126.com>
2018-01-17 17:58:45 +0800

    add persist modules

---
+++ account/OracleHelper.py
@@ -0,0 +1,10 @@
+'''
+Created on 2018.1.17
+
+@author: summer.xia
+@contact: summer_west2010@126.com
+'''
+class OrderHelper(object):
+    def __init__(self, dataSet):
+        print(dataSet)
+        
---
+++ account/SQLiteHelper.py
@@ -0,0 +1,9 @@
+'''
+Created on 2018.1.17
+
+@author: summer.xia
+@contact: summer_west2010@126.com
+'''
+class SQLiteHelper(object):
+    def __init__(self, dataSet):
+        print(dataSet)
