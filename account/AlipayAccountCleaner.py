9820aefb6d69eaa251c3912be9e8b7c05b1c1cbe
xiaxinyu <summer_west2010@126.com>
2018-01-23 19:18:06 +0800

    add Alipay cleaner

---
+++ account/AlipayAccountCleaner.py
@@ -0,0 +1,94 @@
+'''
+Created on 2018.1.23
+
+@author: summer.xia
+@contact: summer_west2010@126.com
+'''
+from account.FileHelper import getFiles
+from account.FileHelper import getAllLines
+from account.StringHelper import isNotEmpty
+from account.FileHelper import generateFile
+
+
+class AlipayAccountCleaner(object):
+    encoding = 'gbk'
+    filterHeadKeyWord = '交易记录明细列表'
+    filterTailKeyWord = '-----------------------------------------'
+    spliter = ','
+    
+    def __init__(self, dataFilesPath=None):
+        self.dataFilesPath = dataFilesPath
+        
+    def getDataLines(self, lines):
+        if len(lines) < 0:
+            return None
+        headFlag = False
+        dataLines = []
+        for line in lines:
+            if self.filterHeadKeyWord in line:
+                headFlag = True
+                continue
+            if self.filterTailKeyWord in line:
+                break
+            if headFlag:                
+                dataLines.append(line)
+        return dataLines
+    
+    def generateMatrix(self, lines):
+        if len(lines) < 0:
+            return None
+        matrix = []
+        headerLength = 0 
+        for (index, line) in enumerate(lines):
+            array = line.split(self.spliter)
+            if index == 0:
+                cleanHeader = filter(isNotEmpty,array)
+                headerLength = len(cleanHeader)
+                matrix.append(cleanHeader)
+            if(len())
+            cleanArray1 = array[0: headerLength]
+            cleanArray2 = []
+            for item in cleanArray1:
+                cleanArray2.append(item.strip())
+            matrix.append(cleanArray2)
+        return matrix
+        
+    def getAllDataLines(self):
+        fileItems = getFiles(self.dataFilesPath)
+        if not fileItems:
+            print('Alipay data files are not available')
+        texts = {}
+        for fileItem in fileItems:
+            lines = getAllLines(fileItem.absolutePath, self.encoding)
+            if lines is None or len(lines) <= 0:
+                continue                
+            dataLines = self.generateMatrix(self.getDataLines(lines))
+            if len(dataLines) <= 0:                continue
+            
+            texts[fileItem.fileName] = dataLines
+        return texts
+    
+    def checkLength(self, dataRows):
+        error = []
+        if dataRows is not None and len(dataRows) > 0:
+            headerLength = 0;
+            for (index, dataRow) in enumerate(dataRows):
+                if index == 0:
+                    headerLength = len(dataRow)
+                    error.append('header length is ' + str(headerLength))
+                else:
+                    if headerLength != len(dataRow):
+                        error.append('error apliay row data position: [' + str(index + 1) + '], length:[ ' + str(len(dataRow)) + ' ]: ' + str(dataRow))
+                        
+        return error
+            
+
+if __name__ == '__main__':
+    dataFilesPath = 'd:\\test\\alipay\\'
+    resultFilePath = 'd://alipay.txt'
+    a = AlipayAccountCleaner(dataFilesPath)
+    result = a.getAllDataLines()    
+    dataRows = result['alipay_record_20180123_1054_1.csv'];
+    generateFile(dataRows, resultFilePath)
+    error = a.checkLength(dataRows)
+    generateFile(error, 'd://alipay-error.txt')
